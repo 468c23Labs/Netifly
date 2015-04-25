461128a3ff17b58086ba56fa25a66db841a20ca2
rschaosid <rschaosid@gmail.com>
2015-04-24 22:06:37 -0500

    stuff

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,9 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/music', function(req, res) {
+  return res.render('music', { artists: [{name: 'Above & Beyond'}] });
+});
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -57,4 +55,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+require('http').createServer(app).listen(3000);
